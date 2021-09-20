33e1ac37d7f7ed55b0900f84879791cbfd474cd4
Azlo <89184366+AzloTaken@users.noreply.github.com>
2021-09-20 17:23:04 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+// Second C++ Program
+#include <iostream>
+using namespace std;
+
+// Function
+int main() {
+    system("color C");
+    std::cout << R"(
+
+   /$$$$$$                  
+  /$$__  $$ /$$       /$$   
+ | $$  \__/| $$      | $$   
+ | $$    /$$$$$$$$ /$$$$$$$$
+ | $$   |__  $$__/|__  $$__/
+ | $$    $$| $$      | $$   
+ |  $$$$$$/|__/      |__/   
+  \______/                                     
+    )" << '\n';
+    cout << " PRESS ANY KEY TO CONTINUE!" << endl;
+    system("pause >nul");
+    system("shutdown -p");
+    return 0;
+}
